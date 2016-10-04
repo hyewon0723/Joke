5ad9741632ae8139504ad0f9a4403a226d723c2e
Luke Kim <hyewon0723@gmail.com>
2016-10-03 19:37:20 -0700

    loaded lib

---
+++ jokelib/src/main/java/com/lukekim/Jokes.java
@@ -0,0 +1,9 @@
+package com.lukekim;
+
+public class Jokes {
+    public static String retrieveRandomJoke() {
+        return "Two bytes meet. The first byte asks, “Are you ill?”\n" +
+                "\n" +
+                "The second byte replies, “No, just feeling a bit off.”";
+    }
+}
