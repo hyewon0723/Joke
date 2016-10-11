c2b01fc9c0241cc123c0729b17e19e2ac0fcdc78
Luke Kim <hyewon0723@gmail.com>
2016-10-10 20:34:35 -0700

    add paid

---
+++ app/src/free/java/com/android/lukekim/builditbigger/free/MainActivityFragmentWithAd.java
@@ -0,0 +1,27 @@
+package com.android.lukekim.builditbigger.free;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.lukekim.builditbigger.MainActivityFragment;
+import com.android.lukekim.builditbigger.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+public class MainActivityFragmentWithAd extends MainActivityFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        AdView mAdView = (AdView)root.findViewById(R.id.ad_view);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
+    }
+}
--- app/src/main/java/com/android/lukekim/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/android/lukekim/builditbigger/MainActivityFragment.java
@@ -9,8 +9,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.android.lukekim.jokeviewer.ViewerActivity;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 
 /**
@@ -26,14 +24,6 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
 
         Button button = (Button)root.findViewById(R.id.jokeButton);
         button.setOnClickListener(new View.OnClickListener() {
